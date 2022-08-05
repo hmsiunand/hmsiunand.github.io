633199e34cb11d67aa40364f55d1e4661421d41e
kemalhiero <kemalhiero@gmail.com>
2022-08-06 00:27:59 +0700

    about

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    // plugins: [require('daisyui')]
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false,
+  },
 }
