a3fdf77925f7904295df2fcf438ccead0fd5c1be
kemalhiero <kemalhiero@gmail.com>
2022-09-30 17:52:01 +0700

    teks welcome

---
+++ js/teksWelcome.js
@@ -0,0 +1,13 @@
+ityped.init("#ityped", {
+  strings: [
+    "Welcome to HMSI",
+    // "Selamat Datang",
+    // "Di Situs HMSI",
+  ],
+  typeSpeed: 55,
+  startDelay: 200,
+  backSpeed: 20,
+  backDelay: 2000,
+  loop: true,
+  cursorChar: "_",
+});
