3f375197fb7e1bd26f185150f683450685846c15
kemalhiero <kemalhiero@gmail.com>
2022-09-30 17:12:38 +0700

    light mode

---
+++ js/teksWelcome.js
@@ -0,0 +1,12 @@
+ityped.init("#ityped", {
+    strings: [
+      "Welcome to HMSI",
+    ],
+    typeSpeed: 55,
+    startDelay: 200,
+    backSpeed: 20,
+    backDelay: 2000,
+    loop: true,
+    cursorChar: "_",
+  });
+  
